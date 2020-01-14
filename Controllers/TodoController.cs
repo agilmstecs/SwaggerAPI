0eca94bf57ed32accf9fdea7a0d2d8d73e507419
Ana <agil@mstecs.com>
2020-01-13 21:10:37 -0600

    Add project files.

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerDemo.Models;
+
+namespace SwaggerDemo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult<TodoItems> Create(TodoItems item)
+        {
+            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult Index()
+        {
+            return CreatedAtRoute("GetTodo", new TodoItems());
+        }
+    }
+}
---
+++ Models/TodoItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace SwaggerDemo.Models
+{
+    public class TodoItems
+    {
+        public long Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DefaultValue(false)]
+        public bool IsComplete { get; set; }
+    }
+}
